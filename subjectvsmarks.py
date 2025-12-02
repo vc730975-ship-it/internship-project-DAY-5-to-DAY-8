81049dc847cd9df481dcbade99a63172762b1764
kallu chaudhary <vc730975@gmail.com>
2025-12-02 16:09:00 +0530

    Create subjectvsmarks.py

---
+++ subjectvsmarks.py
@@ -0,0 +1,8 @@
+subject = ["M1","M2","CHEM","Physics"]
+marks = ["15","19","11","14"]
+
+plt.bar(subject,marks)
+plt.title("Student hrs vs marks")
+plt.xlabel("SUB")
+plt.ylabel("MARKS")
+plt.show()
